a61eb6e724c7e814b1869584d42595f226b34988
amoghaskanda <80926240+amoghaskanda@users.noreply.github.com>
2022-11-20 14:33:31 +0100

    refactor

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 ATM_milano
 ---
-This is a script for scraping live wait times from ATM website. Before running the script, make sure the following dependencies are installed:
-1. Selenium library (Command line: pip install selenium)
-2. webdriver for desired browser: https://www.selenium.dev/documentation/webdriver/getting_started/install_drivers/
-3. After downloading the driver, unzip and (i) move the .exe file to the virtual environment/ folder of the current project OR (ii) change path using os.path() function and run the file
-Alternatively you can click on the .exe file(s) included in the repository
+This is a script for scraping live wait times from ATM website.
+1. Create a virtual environment `virtualenv venv`
+2. Install requirements `pip install -r requirements.txt`
+3. Run `py -m main.py`
 ---
 
 ## Happy scraping!
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logger
+
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,60 +9,55 @@ from time import sleep
 
 def wait_time(fermata):
 
-    #For Chrome/Chromium use webdriver.Chrome()
+    # For Chrome/Chromium use webdriver.Chrome()
     driver = webdriver.Edge()
 
     driver.get("https://giromilano.atm.it/#/home/")
     driver.refresh()
 
-    print("Displaying wait time for first solution only",end="")
+    print("Displaying wait time for first solution only", end="")
     sleep(1)
-    print(".",end="")
+    print(".", end="")
     sleep(1)
-    print(".",end="")
+    print(".", end="")
     sleep(1)
     print(".")
 
-
-    #The following objects primarily wait until the site is loaded or for 10s, whichever comes first.
-    #Then, specific css objects are assigned each object before performing an operation
-    #For example, cercafermata first locates the find stop dropdown menu, and then clicks to open it
-    cerca_fermata = WebDriverWait(driver,10)\
+    cerca_fermata = WebDriverWait(driver, 10)\
         .until(EC.element_to_be_clickable((By.CSS_SELECTOR,"span[translate='lblCercaFermata']")))
 
     cerca_fermata.click()
 
-    #locating input
+    # Locating input
     fermata_input = WebDriverWait(driver,10)\
         .until(EC.element_to_be_clickable((By.CSS_SELECTOR,"input[id='txtFermata']")))
 
-    #sending our function's parameters as input to the text box
+    # Sending our function's parameters as input to the text box
     fermata_input.send_keys(fermata)
     fermata_input.send_keys(Keys.ENTER)
 
-    #clicking only on the first solution (can be changed later)
-    first_solution = WebDriverWait(driver,10)\
+    # Clicking only on the first solution (can be changed later)
+    first_solution = WebDriverWait(driver, 10)\
         .until(EC.element_to_be_clickable((By.CSS_SELECTOR,"div[class='rowLinea']")))
     first_solution.click()
 
-    #Printing the stop name and route number
-    stop_name = WebDriverWait(driver,10)\
-                .until(EC.element_to_be_clickable((By.CSS_SELECTOR,"div[class='divPopupLine ng-binding']")))
+    # Printing the stop name and route number
+    stop_name = WebDriverWait(driver, 10)\
+        .until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[class='divPopupLine ng-binding']")))
     print(stop_name.text)
 
-    #Printing wait time every 30s
-    while(1):
+    # Printing wait time every 30s
+    try:
+        waiting_time = WebDriverWait(driver, 10)\
+            .until(EC.element_to_be_clickable
+                                              ((By.CSS_SELECTOR, "span[class ='fontBold pull-right ng-binding']")))\
+            .text
 
-        try:
-            refresh = WebDriverWait(driver,10)\
-                .until(EC.element_to_be_clickable((By.CSS_SELECTOR,"span[id ='btnRefreshWaitMessage']"))).click()
-            waiting_time = WebDriverWait(driver,10)\
-                .until(EC.element_to_be_clickable((By.CSS_SELECTOR,"span[class ='fontBold pull-right ng-binding']"))).text
+        print("Waiting time: " + waiting_time)
+    except Exception as e:
+        logger.error(e, msg="")
 
-            print("Waiting time: " + waiting_time)
-        except: RuntimeError("No data received")
-        sleep(30)
 
 if __name__ == '__main__':
-    fermata =input("Enter stop/via/piazza: ")
+    fermata = input("Enter stop/via/piazza: ")
     wait_time(fermata)
